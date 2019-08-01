4ccec553c8cd029e3aeabc262a151e25dd66a766
Chintan Vijan <35058356+chintanvijan@users.noreply.github.com>
2019-08-01 13:10:46 +0530

    Add files via upload

--- python/linked-lists.py
+++ python/linked-lists.py
@@ -23,31 +23,41 @@ class linkedlist:
 
 	def insend(self,dataval):
 		val = self.headval
-		while val is not None:
-			prev = val
-			val = val.nextval
-		t = node(dataval)
-		prev.nextval = t
+		if val == None:
+			val = node(dataval)
+			val.nextval = None 
+		else:
+			while val is not None:
+				prev = val
+				val = val.nextval
+			t = node(dataval)
+			prev.nextval = t
 
 	def delbeg(self):
 		self.headval = self.headval.nextval
 
-		
+ch='y'
 llist = linkedlist()
-llist.headval = node("Sunday")
-v1 = node("Monday")
-llist.headval.nextval = v1
-v2 = node("Tuesday")
-v1.nextval = v2 
-#llist.traverse()
-llist.insbeg("Saturday")
-#llist.traverse()
-llist.insend("Wednesday")
-#llist.traverse()
-llist.delbeg()
-#llist.traverse()
-llist.delend()
-llist.traverse()
+while ch=='y':
+	print("Enter 1 to insert a at beginning node into linked list")
+	print("Enter 2 to insert a node at last into linked list")
+	print("Enter 3 to delete a node from linked list")
+	print("Enter 4 to traverse the linked list")
+	inp = int(input())
+	if inp == 1:
+		dataval = input("Enter data value to be inserted")
+		llist.insbeg(dataval)
+		print("Successfully inserted node")
+	if inp ==2:
+		dataval = input("Enter data value to be inserted")
+		llist.insend(dataval)
+		print("Successfully inserted node")
+	if inp ==3:
+		llist.delbeg()
+		print("Successfully deleted node")
+	if inp == 4:
+		llist.traverse()
+	ch = input("Want to continue?(y/n)")
 
 
 
