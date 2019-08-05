5ce10bc659c0e825938a757bcefc3fa431470d70
Chintan Vijan <35058356+chintanvijan@users.noreply.github.com>
2019-08-05 22:08:54 +0530

    Add files via upload

--- python/binary-tree.py
+++ python/binary-tree.py
@@ -19,6 +19,18 @@ class node:
 		else:
 			self.data = data
 
+	def search_value(self,val):
+		if val<self.data:
+			if self.left == None:
+				return str(val)+":Not Found"
+			return self.left.search_value(val)
+		elif val > self.data:
+			if self.right == None:
+				return str(val)+":Not Found"
+			return self.right.search_value(val)
+		else:
+			return print(str(val)+":Found")
+
 	def traverse(self):
 		
 		if self.left:
@@ -32,10 +44,24 @@ ch = 'y'
 while ch == 'y':
 	print("Enter 1 to insert node")
 	print("Enter 2 to traverse")
+	print("Enter 3 to Search in tree")
 	inp = int(input())
 	if inp == 1:
 		data = int(input("Enter data to be inserted:"))
 		root.insert(data)
 	if inp == 2:
 		root.traverse()
+	if inp == 3:
+		val = int(input("Enter value to be searched:"))
+		print(root.search_value(val))
 	ch = input("Want to continue?(y/n)")
+
+
+
+
+
+
+
+
+
+
